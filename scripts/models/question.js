45d9c16657b04cff08c2066f626b3b7bef2109a5
Marcus Christie <marc.christie@gmail.com>
2013-01-08 15:44:36 -0500

    added single vote restriction

--- scripts/models/question.js
+++ scripts/models/question.js
@@ -3,10 +3,29 @@ App.Models.Question = Backbone.Model.extend({
     votes: []
   },
 
+  constrain: function(value, min, max) {
+    if(value < min) return min;
+    if(value > max) return max;
+    return value;
+  },
+
+  getExistingVote: function(userName) {
+    return _(this.get('votes')).find(function(vote) {
+      return vote.voter == userName;
+    });
+  },
+
   vote: function(userName, voteType) {
 
     var voteValue = voteType === 'up' ? 1 : -1;
-    this.get('votes').push({ value: voteValue, voter: userName});
+    var existingVote = this.getExistingVote(userName);
+
+    if(existingVote){
+      existingVote.value = this.constrain(existingVote.value + voteValue, -1, 1);
+    } else {
+      this.get('votes').push({ value: voteValue, voter: userName});
+    }
+
     // Save to persistence layer
     this.save();
   },
